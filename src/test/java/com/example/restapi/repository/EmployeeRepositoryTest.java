126f58742ad0353271ec413f1d70e59f06fea0cb
Marcus Ho <marcus.ho@oocl.com>
2021-12-08 20:54:25 +0800

    [RestApiPractice2][marcus] testcase: should_return_companies_when_findAll_given_companies

--- src/test/java/com/example/restapi/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/restapi/repository/EmployeeRepositoryTest.java
@@ -4,7 +4,6 @@ import com.example.restapi.entity.Employee;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Spy;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
