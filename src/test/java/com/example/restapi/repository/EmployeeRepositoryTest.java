8b6890ff5c7f2534580cec1c3c58eaf7834a1ecf
Marcus Ho <marcus.ho@oocl.com>
2021-12-08 20:43:21 +0800

    [RestApiPractice2][marcus] testcase: should_return_changed_employee_when_save_given_employee

--- src/test/java/com/example/restapi/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/restapi/repository/EmployeeRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.restapi.entity.Employee;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Spy;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ public class EmployeeRepositoryTest {
     @InjectMocks
     EmployeeRepository employeeRepository;
 
-    private List<Employee> getEmployees() {
+    private List<Employee> getExpectedEmployees() {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(1, "Marcus", 22, "Male", 298912220, 1));
         employees.add(new Employee(2, "Marcus2", 22, "Male", 298912220, 1));
@@ -32,52 +33,97 @@ public class EmployeeRepositoryTest {
     @Test
     void should_return_employees_when_findAll_given_employees() {
         //given
-        List<Employee> employees = getEmployees();
+        List<Employee> expectedEmployees = getExpectedEmployees();
 
         //when
         List<Employee> actual = employeeRepository.findAll();
 
         //then
-        assertEquals(employees.size(), actual.size());
-        assertEquals(employees.get(0).getId(), actual.get(0).getId());
-        assertEquals(employees.get(1).getId(), actual.get(1).getId());
-        assertEquals(employees.get(2).getId(), actual.get(2).getId());
-        assertEquals(employees.get(3).getId(), actual.get(3).getId());
-        assertEquals(employees.get(4).getId(), actual.get(4).getId());
-        assertEquals(employees.get(5).getId(), actual.get(5).getId());
-        assertEquals(employees.get(6).getId(), actual.get(6).getId());
+        assertEquals(expectedEmployees.size(), actual.size());
+        assertEquals(expectedEmployees.get(0).getId(), actual.get(0).getId());
+        assertEquals(expectedEmployees.get(1).getId(), actual.get(1).getId());
+        assertEquals(expectedEmployees.get(2).getId(), actual.get(2).getId());
+        assertEquals(expectedEmployees.get(3).getId(), actual.get(3).getId());
+        assertEquals(expectedEmployees.get(4).getId(), actual.get(4).getId());
+        assertEquals(expectedEmployees.get(5).getId(), actual.get(5).getId());
+        assertEquals(expectedEmployees.get(6).getId(), actual.get(6).getId());
     }
 
     @Test
     void should_return_employee_when_findById_given_employee_and_id() {
         //given
-        List<Employee> employees = getEmployees();
+        List<Employee> expectedEmployees = getExpectedEmployees();
 
         //when
         Employee actual = employeeRepository.findById(1);
 
         //then
         assertNotNull(actual);
-        assertEquals(employees.get(0).getId(), actual.getId());
+        assertEquals(expectedEmployees.get(0).getId(), actual.getId());
     }
 
     @Test
     void should_return_employees_when_findByGender_given_employees_and_gender() {
         //given
-        List<Employee> employees = getEmployees();
+        List<Employee> expectedEmployees = getExpectedEmployees();
 
         //when
         List<Employee> actual = employeeRepository.findByGender("Male");
 
         //then
-        assertEquals(employees.size(), actual.size());
-        assertEquals(employees.get(0).getId(), actual.get(0).getId());
-        assertEquals(employees.get(1).getId(), actual.get(1).getId());
-        assertEquals(employees.get(2).getId(), actual.get(2).getId());
-        assertEquals(employees.get(3).getId(), actual.get(3).getId());
-        assertEquals(employees.get(4).getId(), actual.get(4).getId());
-        assertEquals(employees.get(5).getId(), actual.get(5).getId());
-        assertEquals(employees.get(6).getId(), actual.get(6).getId());
+        assertEquals(expectedEmployees.size(), actual.size());
+        assertEquals(expectedEmployees.get(0).getId(), actual.get(0).getId());
+        assertEquals(expectedEmployees.get(1).getId(), actual.get(1).getId());
+        assertEquals(expectedEmployees.get(2).getId(), actual.get(2).getId());
+        assertEquals(expectedEmployees.get(3).getId(), actual.get(3).getId());
+        assertEquals(expectedEmployees.get(4).getId(), actual.get(4).getId());
+        assertEquals(expectedEmployees.get(5).getId(), actual.get(5).getId());
+        assertEquals(expectedEmployees.get(6).getId(), actual.get(6).getId());
+
     }
+
+    @Test
+    void should_return_employees_when_findByPage_given_employees_and_page_and_pageSize() {
+        //given
+        List<Employee> expectedEmployees = getExpectedEmployees();
+
+        //when
+        List<Employee> actual = employeeRepository.findByPage(1, 2);
+
+        //then
+        assertEquals(2, actual.size());
+        assertEquals(expectedEmployees.get(0).getId(), actual.get(0).getId());
+        assertEquals(expectedEmployees.get(1).getId(), actual.get(1).getId());
+    }
+
+    @Test
+    void should_return_created_employee_when_create_given_employee() {
+        //given
+        Employee employee = new Employee(1, "SOME", 19, "Female", 1212, 1);
+        //when
+        Employee actual = employeeRepository.create(employee);
+
+        //then
+        assertNotNull(actual);
+        assertEquals(8, actual.getId());
+    }
+
+    @Test
+    void should_return_changed_employee_when_save_given_employee() {
+        //given
+
+        //when
+        Employee actual = employeeRepository.save(2, new Employee(2, "SOME", 19, "Female", 1212, 1));
+
+        //then
+        assertNotNull(actual);
+        assertEquals(2, actual.getId());
+        assertEquals("SOME", actual.getName());
+        assertEquals(19, actual.getAge());
+        assertEquals("Female", actual.getGender());
+        assertEquals(1212, actual.getSalary());
+        assertEquals(1, actual.getCompanyId());
+    }
+
 
 }
